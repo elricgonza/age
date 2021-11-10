330a8428acf5ed315bc6f754ff78595c18646528
elricgonza <elricgonza@gmail.com>
2021-11-10 16:37:47 -0400

    cont html/js...

--- static/js/uts/buildmap.js
+++ static/js/uts/buildmap.js
@@ -101,7 +101,7 @@ function getgeo(event) {
                 document.getElementById("icircun").setAttribute("value", data.nrocircun)
             });
           
-        buildMapCoord($('input[name="latitud"]').val(),   $('input[name="longitud"]').val());
+        buildMap($('input[name="latitud"]').val(),   $('input[name="longitud"]').val());
     };
 } //getgeo
 
@@ -242,14 +242,8 @@ function buildMapCoord(lat,lon)  {
       longitud: $('input[name="longitud"]').val()
   }, function(data) {
     /*
-      document.getElementById("ideploc").setAttribute("value", 'dd')
       document.getElementById("idepartamento").setAttribute("value", data.gj_ptos)
-      document.getElementById("iprovloc").setAttribute("value", 'pp')
-      document.getElementById("iprovincia").setAttribute("value", 'ddpp')
-      document.getElementById("isecloc").setAttribute("value", 'ss')
-      document.getElementById("imunicipio").setAttribute("value", 'ddss')
-      document.getElementById("icircun").setAttribute("value", 'cc')
     */
-    alert(data.gj_ptos[1]);
+    alert(data.gj_ptos.name);
   });
 }
