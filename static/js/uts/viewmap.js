04098635c3b0cc50f64e828cb63f05a9775d34cb
elricgonza <elricgonza@gmail.com>
2021-11-16 11:09:40 -0400

    ok gj_* en viewmap taJ

--- static/js/uts/viewmap.js
+++ static/js/uts/viewmap.js
@@ -1,193 +1,9 @@
-// ---------------------------------------------------------was  viewmap.js
-    // Set style function that sets fill color property
-    function style(feature) {
-        return {
-            fillColor: 'green', 
-            fillOpacity: 0.5,  
-            weight: 2,
-            opacity: 1,
-            color: '#ffffff',
-            dashArray: '3'
-        };
-    }
+function viewMap(gj_mun, gj_prov, gj_cir) {
+    document.getElementById('dmap').innerHTML = "<div id='map' style='width: 100%; height: 100%;'></div>";
 
-    function styleProv(feature) {
-        return {
-            fillColor: 'orange', 
-            fillOpacity: 0.5,  
-            weight: 2,
-            opacity: 1,
-            color: '#ffffff',
-            dashArray: '3'
-        };
-    }
-
-    function styleCir(feature) {
-        return {
-            fillColor: 'gray', 
-            fillOpacity: 0.5,  
-            weight: 2,
-            opacity: 1,
-            color: 'red',
-            dashArray: '3'
-        };
-    }
-
-
-    var highlight = {
-      'fillColor': 'yellow',
-      'weight': 2,
-      'opacity': 2
-    };
-    
-
-    var highlightCir = {
-      'fillColor': '#a3c2c2',
-      'weight': 2,
-      'opacity': 2
-    };
-
-
-    function forEachFeature(feature, layer) {
-        var popupContent = "<p><b>ID: </b>"+ feature.properties.id +
-            "</br>LOCALIDAD: "+ feature.properties.nom_localidad + '</p>';
-
-        layer.bindPopup(popupContent);
-
-        layer.on("click", function (e) { 
-            locLayer.setStyle(style); //resets layer colors
-            layer.setStyle(highlight);  //highlights selected.
-        }); 
-    }
-
-
-    function forEachFeatureReci(feature, layer) {
-      var popupContent = "<p><b><span style='color:blue'>Recinto: </b></span>" + '<b><big>' + feature.properties.recinto + '</big></b>' +
-            "</br><b><span style='color:blue'>Idloc/Reci: </b></span>"+ feature.properties.idlocreci + 
-            "</br><b><span style='color:blue'>Dep/Prov/Municipio: </b></span>"+ feature.properties.dpm + 
-            "</br><b><span style='color:blue'>Cod. Dep/Prov/Municipio: </b></span>"+ feature.properties.cod + 
-            "</br><b><span style='color:blue'>Circunscripción: </b></span>"+ feature.properties.circun + 
-            "</br><b><span style='color:blue'>Zona: </b></span>"+ feature.properties.zona + 
-            "</br><b><span style='color:blue'>Dirección:  </b></span>"+ feature.properties.direccion + 
-            '</p>';
-
-        layer.bindPopup(popupContent);
-
-        layer.on("click", function (e) { 
-            reciLayer.setStyle(style); //resets layer colors
-            reciLayer.setStyle(highlight);  //highlights selected.
-        }); 
-    }
-
-
-    function forEachFeatureAsi(feature, layer) {
-        var popupContent = "<p><b><span style='color:blue'>Asiento: </b></span>" + feature.properties.asiento +
-            "</br><b><span style='color:blue'>Idloc: </b></span>"+ feature.properties.idloc + 
-            "</br><b><span style='color:blue'>Dep/Prov/Municipio: </b></span>"+ feature.properties.dpm + 
-            "</br><b><span style='color:blue'>Cod. Dep/Prov/Municipio: </b></span>"+ feature.properties.cod + 
-            "</br><b><span style='color:blue'>Tipo Circunscripción: </b></span>"+ feature.properties.tipo_circun + 
-            '</p>';
-
-        layer.bindPopup(popupContent);
-
-        layer.on("click", function (e) { 
-            reciLayer.setStyle(style); //resets layer colors
-            reciLayer.setStyle(highlight);  //highlights selected.
-        }); 
-    }
-
-
-    function forEachFeatureMun(feature, layer) {
-        var popupContent = "<p><b><span style='color:blue'>Cod.: </span></b>"+ feature.properties.cod +
-          "</br><b><span style='color:blue'>Municipio: </span></b>"+ feature.properties.municipio + '</p>';
-
-        layer.bindPopup(popupContent);
-
-        layer.on("click", function (e) { 
-            munLayer.setStyle(style); //resets layer colors
-            layer.setStyle(highlight);  //highlights selected.
-        }); 
-    }
-      
-
-    function forEachFeatureProv(feature, layer) {
-        var popupContent = "<p><b><span style='color:blue'>Cod.: </span></b>"+ feature.properties.cod +
-          "</br><b><span style='color:blue'>Provincia: </span></b>"+ feature.properties.provincia + '</p>';
-
-        layer.bindPopup(popupContent);
-
-        layer.on("click", function (e) { 
-            provLayer.setStyle(styleProv); //resets layer colors
-            layer.setStyle(highlight);  //highlights selected.
-        }); 
-    }
-
-
-    function forEachFeatureCir(feature, layer) {
-        var popupContent = "<p><b>Circunscripción: </b>"+ feature.properties.circun + "</p";
-
-        layer.bindPopup(popupContent);
-
-        layer.on("click", function (e) { 
-            cirLayer.setStyle(styleCir); //resets layer colors
-            layer.setStyle(highlightCir);  //highlights selected.
-        }); 
-    }
-
-
-    function markerOnClick(e)
-    {
-      var coord = e.latlng.toString().split(',');
-      var lat = coord[0].split('(');
-      var lng = coord[1].split(')');
-      marker.bindPopup('Latitud:: ' + lat[1] + '  Longitud: ' + lng[0]);
-    }
-
-
-// invoked ---------------------------------------------cfg/view/coord in  map--------------------------
-function viewMap() {
-
-    // add markerClusterGroup
-    var markersMCG = L.markerClusterGroup();
-
-
-    //tmp--var locLayer = L.geoJson( {{ geo_json | safe }}, {onEachFeature: forEachFeature, style: style});
-
-    /*
-    // reciLayer
-    var iconReci = L.icon({
-      iconUrl: '../static/iconos/icons8-map-pin-40.png',
-      iconSize: [35, 35]
-    });
-
-    var reciLayer = L.geoJson( {{ gj_reci | safe }}, {
-      pointToLayer: function (feature, latlng) {
-        var lm = L.marker(latlng, {icon: iconReci});
-        markersMCG.addLayer(lm);
-        return lm;
-    },
-      onEachFeature: forEachFeatureReci, style: style
-    });
-
-
-    // asiLayer
-    var iconAsi = L.icon({
-      iconUrl: '../static/iconos/icons8-map-pin-40-red.png',
-      iconSize: [35, 35]
-    });
-
-    var asiLayer = L.geoJson( {{ gj_asi | safe }}, {
-      pointToLayer: function (feature, latlng) {
-        var lma = L.marker(latlng, {icon: iconAsi});
-        return lma;
-    },
-      onEachFeature: forEachFeatureAsi, style: style
-    });
-    */
-
-    var provLayer = L.geoJson( {{ gj_prov | safe }}, {onEachFeature: forEachFeatureProv, style: styleProv});
-    var provLayer = L.geoJson( {{ gj_prov | safe }}, {onEachFeature: forEachFeatureProv, style: styleProv});
-    var cirLayer = L.geoJson( {{ gj_cir | safe }}, {onEachFeature: forEachFeatureCir, style: styleCir});
+    var munLayer = L.geoJson( gj_mun , {onEachFeature: forEachFeatureMun, style: style});
+    var provLayer = L.geoJson( gj_prov , {onEachFeature: forEachFeatureProv, style: styleProv});
+    var cirLayer = L.geoJson(  gj_cir , {onEachFeature: forEachFeatureCir, style: styleCir});
 
 
     // baseMaps - overlayMaps
@@ -359,6 +175,7 @@ function viewMap() {
     map.addControl(searchControlMun);
 
 
+/*
     // to reciLayer Search
     var searchControlReci = new L.Control.Search({
       layer: reciLayer,
@@ -370,7 +187,7 @@ function viewMap() {
       position: 'topright'
     });
     map.addControl(searchControlReci);   // add control & checked locLayer
-
+*/
 
     //Plugin magic goes here! Note that you cannot use the same layer object again, as that will confuse the two map controls
     var osm2 = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 13, attribution: osmAttrib });
@@ -411,4 +228,156 @@ function viewMap() {
         marker.on('click', markerOnClick);
       }
     );
+
+  alert('rarooo1..');
+
 }
+
+//--------------------------------------------------------------------------------------------
+
+// ---------------------------------------------------------was  viewmap.js
+    // Set style function that sets fill color property
+    function style(feature) {
+        return {
+            fillColor: 'green', 
+            fillOpacity: 0.5,  
+            weight: 2,
+            opacity: 1,
+            color: '#ffffff',
+            dashArray: '3'
+        };
+    }
+
+    function styleProv(feature) {
+        return {
+            fillColor: 'orange', 
+            fillOpacity: 0.5,  
+            weight: 2,
+            opacity: 1,
+            color: '#ffffff',
+            dashArray: '3'
+        };
+    }
+
+    function styleCir(feature) {
+        return {
+            fillColor: 'gray', 
+            fillOpacity: 0.5,  
+            weight: 2,
+            opacity: 1,
+            color: 'red',
+            dashArray: '3'
+        };
+    }
+
+
+    var highlight = {
+      'fillColor': 'yellow',
+      'weight': 2,
+      'opacity': 2
+    };
+    
+
+    var highlightCir = {
+      'fillColor': '#a3c2c2',
+      'weight': 2,
+      'opacity': 2
+    };
+
+
+    function forEachFeature(feature, layer) {
+        var popupContent = "<p><b>ID: </b>"+ feature.properties.id +
+            "</br>LOCALIDAD: "+ feature.properties.nom_localidad + '</p>';
+
+        layer.bindPopup(popupContent);
+
+        layer.on("click", function (e) { 
+            locLayer.setStyle(style); //resets layer colors
+            layer.setStyle(highlight);  //highlights selected.
+        }); 
+    }
+
+/*
+    function forEachFeatureReci(feature, layer) {
+      var popupContent = "<p><b><span style='color:blue'>Recinto: </b></span>" + '<b><big>' + feature.properties.recinto + '</big></b>' +
+            "</br><b><span style='color:blue'>Idloc/Reci: </b></span>"+ feature.properties.idlocreci + 
+            "</br><b><span style='color:blue'>Dep/Prov/Municipio: </b></span>"+ feature.properties.dpm + 
+            "</br><b><span style='color:blue'>Cod. Dep/Prov/Municipio: </b></span>"+ feature.properties.cod + 
+            "</br><b><span style='color:blue'>Circunscripción: </b></span>"+ feature.properties.circun + 
+            "</br><b><span style='color:blue'>Zona: </b></span>"+ feature.properties.zona + 
+            "</br><b><span style='color:blue'>Dirección:  </b></span>"+ feature.properties.direccion + 
+            '</p>';
+
+        layer.bindPopup(popupContent);
+
+        layer.on("click", function (e) { 
+            reciLayer.setStyle(style); //resets layer colors
+            reciLayer.setStyle(highlight);  //highlights selected.
+        }); 
+    }
+*/
+/*
+    function forEachFeatureAsi(feature, layer) {
+        var popupContent = "<p><b><span style='color:blue'>Asiento: </b></span>" + feature.properties.asiento +
+            "</br><b><span style='color:blue'>Idloc: </b></span>"+ feature.properties.idloc + 
+            "</br><b><span style='color:blue'>Dep/Prov/Municipio: </b></span>"+ feature.properties.dpm + 
+            "</br><b><span style='color:blue'>Cod. Dep/Prov/Municipio: </b></span>"+ feature.properties.cod + 
+            "</br><b><span style='color:blue'>Tipo Circunscripción: </b></span>"+ feature.properties.tipo_circun + 
+            '</p>';
+
+        layer.bindPopup(popupContent);
+
+        layer.on("click", function (e) { 
+            reciLayer.setStyle(style); //resets layer colors
+            reciLayer.setStyle(highlight);  //highlights selected.
+        }); 
+    }
+*/
+
+
+    function forEachFeatureMun(feature, layer) {
+        var popupContent = "<p><b><span style='color:blue'>Cod.: </span></b>"+ feature.properties.cod +
+          "</br><b><span style='color:blue'>Municipio: </span></b>"+ feature.properties.municipio + '</p>';
+
+        layer.bindPopup(popupContent);
+
+        layer.on("click", function (e) { 
+            munLayer.setStyle(style); //resets layer colors
+            layer.setStyle(highlight);  //highlights selected.
+        }); 
+    }
+      
+
+    function forEachFeatureProv(feature, layer) {
+        var popupContent = "<p><b><span style='color:blue'>Cod.: </span></b>"+ feature.properties.cod +
+          "</br><b><span style='color:blue'>Provincia: </span></b>"+ feature.properties.provincia + '</p>';
+
+        layer.bindPopup(popupContent);
+
+        layer.on("click", function (e) { 
+            provLayer.setStyle(styleProv); //resets layer colors
+            layer.setStyle(highlight);  //highlights selected.
+        }); 
+    }
+
+
+    function forEachFeatureCir(feature, layer) {
+        var popupContent = "<p><b>Circunscripción: </b>"+ feature.properties.circun + "</p";
+
+        layer.bindPopup(popupContent);
+
+        layer.on("click", function (e) { 
+            cirLayer.setStyle(styleCir); //resets layer colors
+            layer.setStyle(highlightCir);  //highlights selected.
+        }); 
+    }
+
+
+    function markerOnClick(e)
+    {
+      var coord = e.latlng.toString().split(',');
+      var lat = coord[0].split('(');
+      var lng = coord[1].split(')');
+      marker.bindPopup('Latitud:: ' + lat[1] + '  Longitud: ' + lng[0]);
+    }
+
