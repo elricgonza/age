75dd51f325194f8a5e379ec08da66ba757ac13c2
elricgonza <elricgonza@gmail.com>
2024-10-10 14:57:29 -0400

    test migra

--- migra08.py
+++ migra08.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-''' Migra a MsSQL Server 2008 '''
+''' Migra a MsSQL Server 2008 
+    Previo check inexistencia de triggers  - test 
+    no migradas: Circun, clasifGrupo y PAIS
+    tested 10 oct/2024
+'''
 
 import pymssql as mss
 
@@ -12,7 +16,7 @@ import datetime
 
 def get_db_ms():
     #pms = ("localhost","sa","123qweAS","GeografiaElectoral_app")
-    pms = ("10.100.15.54","appgeo","1234qweAS","GeografiaElectoral_app")
+    #pms = ("10.100.15.54","appgeo","1234qweAS","GeografiaElectoral_app")
     try:
         cx = mss.connect(*pms)
         print("cnx mssql ok -app-")
@@ -32,22 +36,77 @@ def get_db_08():
         print("Error en conexión... -GeografiaElectoral_mig-")
 
 
-def migra():
-    global cxms
-    global cx08
-    cxms = get_db_ms()
-    cx08 = get_db_08()
+def migresto():
+    # clasif
+    x = datetime.datetime.now()
+    print('---CLASIF - Fecha y Hora inicio: = %s' %x)
+    
+    s = 'delete from clasif'
+    cur08.execute(s)
+    cx08.commit()
 
-    curms = cxms.cursor()
-    cur08 = cx08.cursor()
+    s = 'select * from geografiaElectoral_app.dbo.clasif'
+    curms.execute(s)
+    rows = curms.fetchall()
 
+    n = 0
+    for row in rows:
+        s = """
+            insert into clasif (idClasif, descripcion, clasifGrupoId, clasifSubGrupo)
+                values (%s, %s, %s, %s)
+        """
+        cur08.execute(s, (
+            row[0], row[1], row[2], row[3] )) 
+        n+= 1
+        if n % 50 == 0:
+            print(n)
+    cx08.commit()
+
+    x = datetime.datetime.now()
+    print(n)
+    print('---END CLASIF Fecha y Hora End: = %s' %x)
+
+    # check si necesario
     '''
+    # clasifGrupo
+    x = datetime.datetime.now()
+    print('---clasifGrupo - Fecha y Hora inicio: = %s' %x)
+    
+    s = 'delete from clasifGrupo'
+    cur08.execute(s)
+    cx08.commit()
+
+    s = 'select * from geografiaElectoral_app.dbo.clasifGrupo'
+    curms.execute(s)
+    rows = curms.fetchall()
+
+    n = 0
+    for row in rows:
+        s = """
+            insert into clasifGrupo (idClasifGrupo, descripcion)
+                values (%s, %s)
+        """
+        cur08.execute(s, (
+            row[0], row[1] )) 
+        n += 1
+        if n % 50 == 0:
+            print(n)
+    cx08.commit()
+
+    x = datetime.datetime.now()
+    print(n)
+    print('---END clasifGrupo Fecha y Hora End: = %s' %x)
+    '''
+ 
     # dep
+    x = datetime.datetime.now()
+    print('---DEP - Fecha y Hora inicio: = %s' %x)
+
     s = 'delete from dep'
     cur08.execute(s)
     cx08.commit()
 
-    s = 'select * from dep'
+    s = 'select * from geografiaElectoral_app.dbo.dep'
     curms.execute(s)
     rows = curms.fetchall()
 
@@ -63,15 +122,153 @@ def migra():
             row[0], row[1], row[2], row[3], row[4], 
             row[5], row[6], row[7], row[8] ))
         n+= 1
-        print(n)
+        if n % 50 == 0:
+            print(n)
     cx08.commit()
+    x = datetime.datetime.now()
+    print('---END DEP - Fecha y Hora End: = %s' %x)
+
+
+    # prov
+    x = datetime.datetime.now()
+    print('---PROV - Fecha y Hora inicio: = %s' %x)
+
+    s = 'delete from prov'
+    cur08.execute(s)
+    cx08.commit()
+
+    s = 'select * from geografiaElectoral_app.dbo.prov'
+    curms.execute(s)
+    rows = curms.fetchall()
+
+    n = 0
+    for row in rows:
+        s = """
+            insert into prov (DepProv, Prov, NomProv, codprov, 
+                fechaIngreso, fechaAct, usuario, descNivelId)
+                values (%s, %s, %s, %s, 
+                %s, %s, %s, %s)
+        """
+        cur08.execute(s, (
+            row[0], row[1], row[2], row[3], 
+            row[4], row[5], row[6], row[7] ))
+        n+= 1
+        if n % 50 == 0:
+            print(n)
+    cx08.commit()
+    x = datetime.datetime.now()
+    print(n)
+    print('---END PROV - Fecha y Hora End: = %s' %x)
+
+
+    # sec
+    x = datetime.datetime.now()
+    print('---SEC - Fecha y Hora inicio: = %s' %x)
+
+    s = 'delete from sec'
+    cur08.execute(s)
+    cx08.commit()
+
+    s = 'select * from geografiaElectoral_app.dbo.sec'
+    curms.execute(s)
+    rows = curms.fetchall()
+
+    n = 0
+    for row in rows:
+        s = """
+            insert into sec (DepSec, ProvSec, Sec, NumConceSec,  NomSec, 
+                CircunSec, CodProv, CodSecc,  
+                fechaIngreso, fechaAct, usuario, descNivelId)
+                values (%s, %s, %s, %s, %s,
+                %s, %s, %s,
+                %s, %s, %s, %s)
+        """
+        cur08.execute(s, (
+            row[0], row[1], row[2], row[3], row[4],
+            row[5], row[6], row[7], 
+            row[8], row[9], row[10], row[11] ))
+        n+= 1
+        if n % 100 == 0:
+            print(n)
+    cx08.commit()
+    x = datetime.datetime.now()
+    print(n)
+    print('---END SEC - Fecha y Hora End: = %s' %x)
+
     
+def migdist():
+    # dist
+    x = datetime.datetime.now()
+    print('---DIST - Fecha y Hora inicio: = %s' %x)
+
+    s = 'delete from dist'
+    cur08.execute(s)
+    cx08.commit()
+
+    s = 'select * from geografiaElectoral_app.dbo.dist'
+    curms.execute(s)
+    rows = curms.fetchall()
+
+    n = 0
+    for row in rows:
+        s = """
+            insert into dist (IdLocDist, Dist, CircunDist, NomDist, 
+                fechaIngreso, fechaAct, usuario)
+                values (%s, %s, %s, %s,
+                %s, %s, %s)
+        """
+        cur08.execute(s, (
+            row[0], row[1], row[2], row[3], 
+            row[4], row[5], row[6]))
+        n+= 1
+        if n % 1000 == 0:
+            print(n)
+    cx08.commit()
+    x = datetime.datetime.now()
+    print(n)
+    print('---END DIST - Fecha y Hora End: = %s' %x)
+
+
+def migzona():
+    # zona
+    x = datetime.datetime.now()
+    print('---ZONA - Fecha y Hora inicio: = %s' %x)
+
+    s = 'delete from zona'
+    cur08.execute(s)
+    cx08.commit()
+
+    s = 'select * from geografiaElectoral_app.dbo.zona'
+    curms.execute(s)
+    rows = curms.fetchall()
+
+    n = 0
+    for row in rows:
+        s = """
+            insert into zona (IdLocZona, Zona, NomZona, DistZona, 
+                fechaIngreso, fechaAct, usuario)
+                values (%s, %s, %s, %s,
+                %s, %s, %s)
+        """
+        cur08.execute(s, (
+            row[0], row[1], row[2], row[3], 
+            row[4], row[5], row[6]))
+        n+= 1
+        if n % 1000 == 0:
+            print(n)
+    cx08.commit()
+    x = datetime.datetime.now()
+    print(n)
+    print('---END ZONA - Fecha y Hora End: = %s' %x)
+
+
+def migreci():
     # reci
     s = 'delete from reci'
     cur08.execute(s)
     cx08.commit()
 
-    s = 'select * from reci'
+    s = 'select * from geografiaElectoral_app.dbo.reci'
     curms.execute(s)
     rows = curms.fetchall()
 
@@ -108,16 +305,17 @@ def migra():
     cx08.commit()
 
     x = datetime.datetime.now()
-    print('---Fecha y Hora End: = %s' %x)
-    '''
+    print('---END RECI - Fecha y Hora End: = %s' %x)
 
-
+    
+def migloc():
     # loc
+    print('---migra LOC---')
     s = 'delete from loc'
     cur08.execute(s)
     cx08.commit()
 
-    s = 'select * from loc'
+    s = 'select * from geografiaElectoral_app.dbo.loc'
     curms.execute(s)
     rows = curms.fetchall()
 
@@ -152,16 +350,27 @@ def migra():
             row[25], row[26], row[27], row[28], row[29], 
             row[30], row[31], row[32], row[33], row[34], 
             row[35], row[36]))
-        n+= 1
-
-        #if (n % 1000 == 0):
-        #    print(n)
+        n += 1
+        if (n % 1000 == 0):
+            print(n)
 
     print(n)
     cx08.commit()
 
     x = datetime.datetime.now()
-    print('---Fecha y Hora End: = %s' %x)
+    print('---END LOC - Fecha y Hora End: = %s' %x)
 
 if __name__ == '__main__':
-    migra()
+    global cxms
+    global cx08
+    cxms = get_db_ms()
+    cx08 = get_db_08()
+
+    curms = cxms.cursor()
+    cur08 = cx08.cursor()
+
+    migresto()
+    migloc()
+    migreci()
+    migdist()
+    migzona()
